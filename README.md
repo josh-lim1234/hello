46f395a96fa2d3fe52b82cc20e700a69f4abb8c5
Susan Buck <susan@susanbuck.net>
2018-09-12 13:46:31 -0400

    Added homepage and the start of CSS

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project Hello World
+# Project Hello World!
 + By: Susan Buck
 + Production URL: <http://hello-world.dwa15.me>
 
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+
+    <title>Hello World</title>
+    <meta charset='utf-8'>
+
+    <link href='/css/main.css' rel='stylesheet'>
+
+</head>
+<body>
+
+    <h1>Hello World</h1>
+
+    <img src='images/fireworks.jpg'>
+    
+</body>
+</html>
